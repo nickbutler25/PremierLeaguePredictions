f98556b97bb19758f302ed2ef9ebe551609e13b7
Nick Butler <nick.w.butler@gmail.com>
2026-05-09 12:50:50 -0400

    Add EF Core migration for Team.MediumName column

---
+++ backend/PremierLeaguePredictions.Infrastructure/Migrations/20260509164912_AddTeamMediumName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PremierLeaguePredictions.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTeamMediumName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MediumName",
+                table: "teams",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MediumName",
+                table: "teams");
+        }
+    }
+}
